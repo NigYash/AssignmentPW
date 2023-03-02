817afd45e751641368f55e8c2e3da5ccb8fe7f30
NigYash <125470058+NigYash@users.noreply.github.com>
2023-03-02 19:45:21 +0530

    Add files via upload

---
+++ Jan29.py
@@ -0,0 +1,55 @@
+Qs1- Who developed python programing language?
+Ans1- Guido Van Rossum
+
+Qs2-Which type of programming does python support?
+Ans2-Object-oriented programing
+
+Qs3-Is python case sesitive when dealing with identifiers?
+Ans3 - Yes
+
+Qs4- What is the correct extension of python file?
+Ans4- ".py"
+
+Qs5 - Is python code compiled or interpreted?
+Ans5- Interpreted
+
+Qs6- Name a few blocks of code used to define in python language?
+Ans6- "if" , "def" , ":" , "for", "while"
+
+Qs7- State a character used to give single-line comments in python?
+Ans7- "#"
+
+Qs8-Mention functions which can help us to find the version of python that we are currently working on?
+Ans8 - Using Command Prompt  "python --version" or
+       import sys
+       print(sys.version)
+
+Qs9-Python supports the creation of anonymous fuctions at runtime, using a construct called
+Ans9-"LAMDA"   
+
+
+Qs10- What does pip stands for?
+ANS10-" Pip Install Packages"
+
+Qs11 - Mention a few built-in functions in python?
+Ans11- "print"
+       "input"
+       "list"
+       "type"
+       "tuple"
+
+Qs12 -  What is the maximum possible length of an identifier in python?
+ANS12 -  Not defined 
+
+Qs13 -What are benefits of using python?
+Ans13-Easy to learn and Use
+      Versatile
+      Libraries and frameworks
+      Large community
+
+Qs14 - How is memory managed in python?
+Ans14- Memory management in Python is handled automatically by a built-in mechanism called the "Python Memory Manager"
+
+
+Qs15 - Is indentation required in python? 
+       Yes , it i srequired in python.
